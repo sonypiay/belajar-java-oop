510201406bdba28c4574948381acfd5fd2af5700
sonypiay <pyscho30@gmail.com>
2024-09-21 01:01:48 +0700

    anonymous class

---
+++ src/programmer/zaman/now/application/HelloWorldApp.java
@@ -0,0 +1,22 @@
+package programmer.zaman.now.application;
+
+import programmer.zaman.now.data.HelloWorld;
+
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        HelloWorld english = new HelloWorld() {
+            @Override
+            public void sayHello() {
+                System.out.println("Hello");
+            }
+
+            @Override
+            public void sayHello(String name) {
+                System.out.println("Hello " + name);
+            }
+        };
+
+        english.sayHello();
+        english.sayHello("Sony");
+    }
+}
