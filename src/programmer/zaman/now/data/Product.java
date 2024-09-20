2546d8eee4a3f4205036140b18fa958f5f6ec943
sonypiay <pyscho30@gmail.com>
2024-09-21 00:38:39 +0700

    equals, hashCode

--- src/programmer/zaman/now/data/Product.java
+++ src/programmer/zaman/now/data/Product.java
@@ -1,5 +1,7 @@
 package programmer.zaman.now.data;
 
+import java.util.Objects;
+
 public class Product {
     protected String name;
     protected int price;
@@ -20,4 +22,20 @@ public class Product {
    public String toString() {
         return "Product name: " + this.name + ", price: " + this.price;
    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+        return price == product.price && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + price;
+        return result;
+    }
 }
--- src/programmer/zaman/now/data/ProductApp.java
+++ src/programmer/zaman/now/data/ProductApp.java
@@ -7,5 +7,12 @@ public class ProductApp {
         System.out.println(product.getName());
         System.out.println(product.getPrice());
         System.out.println(product);
+
+        Product product2 = new Product("Macbook Pro", 3000000);
+        System.out.println(product.equals(product2));
+
+        System.out.println(product.hashCode());
+        System.out.println(product2.hashCode());
+        System.out.println(product.hashCode() == product2.hashCode());
     }
 }
