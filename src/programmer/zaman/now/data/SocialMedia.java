ad19e4dc49a4feb0bc6107977da2fba1a9d1ad11
sonypiay <pyscho30@gmail.com>
2024-09-21 00:43:10 +0700

    final class & final method

---
+++ src/programmer/zaman/now/data/SocialMedia.java
@@ -0,0 +1,17 @@
+package programmer.zaman.now.data;
+
+class SocialMedia {
+    String name;
+}
+
+final class Facebook extends SocialMedia {
+    final void login(String username, String password) {
+
+    }
+}
+
+class FakeFacebook extends Facebook {
+    void login( String username, String Password ) {
+
+    }
+}
